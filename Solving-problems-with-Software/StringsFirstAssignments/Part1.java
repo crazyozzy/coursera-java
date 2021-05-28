44e65492995b04bcc72ed09c8c343df5210efd92
Denis Kurskiy <denis.kurskiy@gmail.com>
2021-05-28 20:45:37 +0300

    create a new class Part1

---
+++ Solving-problems-with-Software/StringsFirstAssignments/Part1.java
@@ -0,0 +1,14 @@
+
+/**
+ * Finding a DNA sequence in string.
+ * 
+ * @crazyozzy aka KDA (your name) 
+ * @0.0.0 (a version number or a date)
+ */
+public class Part1 {
+    public String findSimpleGene (String strDNA){
+        String fndSeq = "";
+        
+        return fndSeq;
+    }
+}
