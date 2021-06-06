df39967369c0784e2f5083263bc6a47519b6b3bb
crazyozzy <crazyozzy001@gmail.com>
2021-06-06 16:10:20 +0300

    add changes to Temp

--- Solving-problems-with-Software/CSV/temperature/Temp.java
+++ Solving-problems-with-Software/CSV/temperature/Temp.java
@@ -74,12 +74,92 @@ public class Temp {
 		return lowestTempFile.getName();
 	}
 
+	public CSVRecord lowestHumidityInManyFiles () {
+		DirectoryResource dr = new DirectoryResource();
+		FileResource lowestHumidityFr = null;
+
+		for (File file : dr.selectedFiles()) {
+			FileResource fr = new FileResource(file);
+			if (lowestHumidityFr != null) {
+				CSVRecord lowestHumidityFileRecord = lowestHumidityInFile(lowestHumidityFr.getCSVParser());
+				CSVRecord frRecord = lowestHumidityInFile(fr.getCSVParser());
+				int humidityInLowestHumidityFrRecord = Integer.parseInt(lowestHumidityFileRecord.get("Humidity"));
+				int humidityInFrRecord = Integer.parseInt(frRecord.get("Humidity"));
+
+				if (humidityInFrRecord < humidityInLowestHumidityFrRecord) {
+					lowestHumidityFr = fr;
+				}
+			}
+			else {
+				lowestHumidityFr = fr;
+			}
+		}
+
+		return lowestHumidityInFile(lowestHumidityFr.getCSVParser());
+	}
+
+	public double averageTempInFile (CSVParser parser) {
+		int counter = 0;
+		double sum = 0;
+
+		for (CSVRecord record : parser) {
+			if ( !("-9999".equals(record.get("TemperatureF"))) ) {
+				sum = sum + Double.parseDouble(record.get("TemperatureF"));
+				counter++;
+			}
+		}
+
+		return (sum / counter);
+	}
+
+	public double averageTempWithHighHumidityInFile (CSVParser parser, int value) {
+		int counter = 0;
+		double averageTemp = 0;
+
+		for (CSVRecord record : parser) {
+			if ( !("N/A".equals(record.get("Humidity"))) && !("-9999".equals(record.get("TemperatureF"))) ) {
+				int humidity = Integer.parseInt(record.get("Humidity"));
+				double temp = Double.parseDouble(record.get("TemperatureF"));
+
+				if ( humidity >= value ) {
+					averageTemp += temp;
+					counter++;
+				}
+			}
+		}
+
+		if ( counter == 0 ) {
+			return -1000;
+		}
+		else {
+			return ( averageTemp / counter );
+		}
+	}
+
+	public void testAverageTempWithHighHumidityInFile () {
+		FileResource fr = new FileResource("./nc_weather/2014/weather-2014-03-20.csv");
+		double averageTemp = averageTempWithHighHumidityInFile(fr.getCSVParser(), 80);
+
+		if ( averageTemp < -999 ) {
+			System.out.println("No temperatures with that humidity");
+		}
+		else {
+			System.out.println("Average temp when high humidity is " + averageTemp);
+		}
+	}
+
+	public void testAverageTempInFile () {
+		FileResource fr = new FileResource("./nc_weather/2014/weather-2014-01-20.csv");
+		CSVParser parser = fr.getCSVParser();
+		System.out.println("Average temp: " + averageTempInFile(parser));
+	}
+
 	public void testFileWithColdestTemperature () {
 		System.out.println("File name with coldest temp: " + fileWithColdestTemperature());
 	}
 
 	public void testerColdestHourInFile () {
-		FileResource fr = new FileResource("./nc_weather/2014/weather-2014-01-08.csv");
+		FileResource fr = new FileResource("./nc_weather/2014/weather-2014-05-01.csv");
 		CSVParser parser = fr.getCSVParser();
 		CSVRecord lowestRow = coldestHourInFile(parser);
 
@@ -96,8 +176,10 @@ public class Temp {
 
 	public static void main (String[] args) {
 		Temp tmp = new Temp();
-		//tmp.testerColdestHourInFile();
+		tmp.testerColdestHourInFile();
 		//tmp.testFileWithColdestTemperature();
-		tmp.testLowestHumidityInFile();
+		//tmp.testLowestHumidityInFile();
+		//tmp.testAverageTempInFile();
+		//tmp.testAverageTempWithHighHumidityInFile(); 
 	}
 }
